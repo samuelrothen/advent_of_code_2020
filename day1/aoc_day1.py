70cafe404a46e18b1a7a3cc437a294ecf0a39c29
Samuel Rothen <70198663+samuelrothen@users.noreply.github.com>
2020-12-08 19:51:11 +0100

    Initial commit

---
+++ day1/aoc_day1.py
@@ -0,0 +1,28 @@
+# Advent of Code Day 1
+
+import numpy as np
+
+
+def find_sum_two_n(numbers, n_find):
+    for i, n1 in enumerate(numbers):
+        for j, n2 in enumerate(numbers[i+1:]):
+            n_sum = n1 + n2
+            if n_sum == n_find:
+                return n1 * n2
+    return 0
+
+
+def find_sum_three_n(numbers, n_find):
+    for i,n1 in enumerate(numbers):
+        rest = n_find - n1
+        found = find_sum_two_n(numbers[i+1:], rest)
+        if found:
+            return found * n1
+
+
+numbers = np.loadtxt('input/day1.csv',dtype=int)
+n_find = 2020
+
+print(f'Part 1: {find_sum_two_n(numbers, n_find)}')
+print(f'Part 2: {find_sum_three_n(numbers, n_find)}')
+
