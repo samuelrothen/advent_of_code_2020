8c529bb61fadfa78316ba02391da6e9219ca349b
Samuel Rothen <samuel.rothen@gmail.com>
2020-12-15 11:15:14 +0100

    Update aoc_day7.py

--- aoc_day7.py
+++ aoc_day7.py
@@ -10,40 +10,40 @@ def parse_colors(bag_rules):
         bags = re.findall('(\w+ \w+) bag', rule)
         first_bag = bags[0]
         bag_contains = bags[1:]
-        if bag_contains[0] != 'no other':
-            color_rules[first_bag] = bag_contains
+        color_rules[first_bag] = bag_contains
     return color_rules
 
 
-def get_unique_colors(color_rules):
-    unique_colors = set()
-    for key in color_rules:
-        unique_colors.add(key)
-        for value in color_rules[key]:
-            unique_colors.add(value)
-    return unique_colors
 
 
 def search_color(color_rules, color):
-    pass
-
-
-
-for key in color_rules:
-    if 'shiny gold' in color_rules[key]:
+    if color == 'no other':
+        return False
+    elif 'shiny gold' in color_rules[color]:
         return True
     else:
-        for color in color_rules[key]:
-            color_rules[color]
+        for contained_color in color_rules[color]:
+            if search_color(color_rules,contained_color):
+                return True
+
+
+def check(bag_rules):
+    n_found = 0
+    color_rules = parse_colors(bag_rules)
+    for key in color_rules:
+        if search_color(color_rules, key):
+            n_found += 1
+    return n_found
+
 
 
 with open ('input/day7.txt', 'r') as f:
     bag_rules = f.read().split('\n')
 
-color_rules = parse_colors(bag_rules)
-unique_colors = get_unique_colors(color_rules)
 
-
-
-# print(f'Part 1: {1}')
+print(f'Part 1: {check(bag_rules)}')
 # print(f'Part 2: {1}')
+
+
+
+
