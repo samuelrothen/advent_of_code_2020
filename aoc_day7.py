a8d6871b15cfbbe0c7e5e6a8c02e8a026b40ab2e
Samuel Rothen <samuel.rothen@gmail.com>
2020-12-16 11:38:53 +0100

    Update aoc_day7.py

--- aoc_day7.py
+++ aoc_day7.py
@@ -17,19 +17,24 @@ def parse_colors(bag_rules):
     return color_rules, numbers_rules
 
 
-def search_color(color_rules, color):
-    if color == 'no other':
+def search_color(color_rules, check_color):
+    if check_color == 'no other':
         return False
-    elif 'shiny gold' in color_rules[color]:
+    elif 'shiny gold' in color_rules[check_color]:
         return True
     else:
-        for contained_color in color_rules[color]:
-            if search_color(color_rules,contained_color):
+        for contained_color in color_rules[check_color]:
+            if search_color(color_rules, contained_color):
                 return True
 
 
-def sum_bags(bag_rules, color):
-    pass
+def sum_bags(color_rules, numbers_rules, check_color, mulip = 1):
+    colors = color_rules[check_color]
+    n_bags = numbers_rules[check_color]
+    n_bags_total = 0
+    for color, n in zip(colors, n_bags):
+        n_bags_total += n * sum_bags(color_rules, numbers_rules, color, mulip * n)
+    return n_bags_total + 1
 
 
 def check(bag_rules):
@@ -48,8 +53,6 @@ with open ('input/day7.txt', 'r') as f:
 color_rules, numbers_rules = parse_colors(bag_rules)
 
 print(f'Part 1: {check(bag_rules)}')
-# print(f'Part 2: {1}')
-
-
+print(f'Part 2: {sum_bags(color_rules, numbers_rules, "shiny gold", mulip = 1) - 1}')
 
 
