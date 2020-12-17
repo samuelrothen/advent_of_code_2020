31853e8417e0b9624da6bfef740a3196def9a414
Samuel Rothen <70198663+samuelrothen@users.noreply.github.com>
2020-12-17 14:22:46 +0100

    Update aoc_day10.py

--- aoc_day10.py
+++ aoc_day10.py
@@ -1,8 +1,33 @@
 # Advent of Code Day 10
 
 
-with open ('input/day10.txt', 'r') as f:
-    data = [int(line) for line in f.read().splitlines()]
+import numpy as np
 
 
+def get_diff(data):
+    data = np.sort(data)
+    data = np.insert(data, 0, 0)
+    data = np.insert(data, data.size, np.max(data) + 3)
+    diff = np.diff(data)
+    return np.count_nonzero(diff == 1) * np.count_nonzero(diff == 3)
 
+
+def get_arrangements(data):
+    data = np.sort(data)
+    arrang={0:1}
+    for adapt in data:
+        arrang[adapt] = 0
+        if adapt - 1 in arrang:
+            arrang[adapt] += arrang[adapt - 1]
+        if adapt - 2 in arrang:
+            arrang[adapt] += arrang[adapt - 2]
+        if adapt - 3 in arrang:
+            arrang[adapt] += arrang[adapt - 3]
+    return arrang[max(arrang)]
+
+
+data = np.loadtxt('input/day10.txt',dtype=int)
+
+
+print(f'Part 1: {get_diff(data)}')
+print(f'Part 2: {get_arrangements(data)}')
