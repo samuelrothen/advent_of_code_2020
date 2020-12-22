62fa52dbbdd06c81df7f6080105071c72505c001
Samuel Rothen <70198663+samuelrothen@users.noreply.github.com>
2020-12-22 23:31:54 +0100

    Day 14 todo

---
+++ aoc_day14.py
@@ -0,0 +1,18 @@
+# Advent of Code Day 14
+
+
+import numpy as np
+
+
+
+with open ('input/day14.txt', 'r') as f:
+    data =  f.read().split('\n')
+    
+
+
+
+print(f'Part 1: {0}')
+print(f'Part 2: {0}')
+
+
+
