a9af32b7b9ec3fef6ad4e988f63af9f97f53be46
Samuel Rothen <70198663+samuelrothen@users.noreply.github.com>
2021-01-19 18:29:10 +0100

    Update aoc_day20_WIP.py

--- aoc_day20_WIP.py
+++ aoc_day20_WIP.py
@@ -25,13 +25,13 @@ edges_T = {}
 
 for num, tile in tiles.items():
     T = [tile,
-         np.flip(tile, axis=0),
-         np.flip(tile, axis=1),
-         np.rot90(tile, k=1),
-         np.flip(np.rot90(tile, k=1), axis=0),
-         np.flip(np.rot90(tile, k=1), axis=1),
-         np.rot90(tile, k=2),
-         np.rot90(tile, k=3)]
+         np.flip(tile, axis = 0),
+         np.flip(tile, axis = 1),
+         np.rot90(tile, k = 1),
+         np.flip(np.rot90(tile, k = 1), axis = 0),
+         np.flip(np.rot90(tile, k = 1), axis = 1),
+         np.rot90(tile, k = 2),
+         np.rot90(tile, k = 3)]
 
     E = [tile[0, :],
          tile[-1, :],
@@ -50,7 +50,7 @@ for (num1, edges1), (num2, edges2) in product(edges_T.items(), edges_T.items()):
     if num1 == num2:
         continue
     for e1, e2 in product(edges1, edges2[:4]):
-        if abs((e1-e2)).sum() == 0:
+        if abs((e1 - e2)).sum() == 0:
             n_matches[num1] += 1
 
 result = 1
